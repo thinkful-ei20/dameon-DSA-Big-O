232cdf1086c46685f875f3b36a5e8f8db2ab604a
Dameon1 <dameonmedoza@gmail.com>
2018-06-19 10:00:17 -0700

    first set of drills

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-let counter = {
-  ticks:0
-};
+let counter = 0;
 
 
 
@@ -18,6 +16,10 @@ function isEven(value){
 }
 
 // Are you here?----------------------------------------------
+//--This function is O(n^2)
+//--First loop goes through say (1000x) and for every time it loops through It will loop through the
+//second array 1000x
+
 function areYouHere(arr1, arr2) {
   for (let i=0; i<arr1.length; i++) {
     const el1 = arr1[i];
@@ -29,7 +31,14 @@ function areYouHere(arr1, arr2) {
   return false;
 }
 
+let arr1 = [1,2,3,4,5,];
+let arr2 = [2,4,6,8,1];
+
+
 // Doubler----------------------------------------------------
+//--This function is O(n) 
+//-- It performs 1 function in direct size to the array input
+
 function doubleArrayValues(array) {
   for (let i=0; i<array.length; i++) {
     array[i] *= 2;
@@ -38,6 +47,10 @@ function doubleArrayValues(array) {
 }
 
 // Naive Search-----------------------------------------------
+// O(1) vs. O(log n) vs. O(n) vs. O(n log(n)) vs. O(n^2) vs. O(2^n) vs. O(n!)
+//--This function is O(n)
+//--It performs a single function in direct corealation to the input size
+
 function naiveSearch(array, item) {
   for (let i=0; i<array.length; i++) {
     if (array[i] === item) {
@@ -47,42 +60,56 @@ function naiveSearch(array, item) {
 }
 
 // Creating pairs---------------------------------------------
+// O(1) vs. O(log n) vs. O(n) vs. O(n log(n)) vs. O(n^2) vs. O(2^n) vs. O(n!)
+//-- This function is O(n^2) 
+//-- This first loop will run the second loop the amount of times in direct length to the array 
+//-- example (1 loop = 1 operation), (2 loops = 4 ops ) , (3 loops = 9 ops)
+
 function createPairs(arr) {
   for (let i = 0; i < arr.length; i++) {
     for(let j = i+1; j < arr.length; j++) {
-      console.log(arr[i] + ', ' +  arr[j] );
+      counter+=1;
+      // console.log(arr[i] + ', ' +  arr[j] );
     }
   }
+  console.log(counter);
+  counter = 0;
 }
 
+
+//    if arr.length is 5 .. operations performed is? = 10 = 25
+//    if arr.length is 10 .. operations performed is? = 45 = 100
+//    if arr.length is 100 .. operations performed is? = 4950 = 10000
+//createPairs(100);
+
+
 // Fibonacci--------------------------------------------------
+//--This function is O(n)
+//--It runs the same amount of operations in direct increase of (n)
+// examples (2 loops = 2 functions), (4 loops = 4 functions), (10 loops = 10 functions)
 function generateFib(num) {
   let result = [];
   for (let i = 1; i <= num; i++) {
 
-    // we're adding the first item
-    // to the result list, append the
-    // number 0 to results
     if (i === 1) {
       result.push(0);
     }
-    // ...and if it's the second item
-    // append 1
     else if (i == 2) {
       result.push(1);
     }
 
-    // otherwise, sum the two previous result items, and append that value to results.
     else {
       result.push(result[i - 2] + result[i - 3]);
     }
   }
-  // once the for loop finishes
-  // we return `result`.
+ 
   return result;
 }
 
 // Effecient Search----------------------------------------------
+//--This function is O(log n)
+//--This function takes an array and divides it in half
+//--Example lengths (10 = 5) , (20 = 10)
 function efficientSearch(array, item) {
   let minIndex = 0;
   let maxIndex = array.length - 1;
@@ -107,11 +134,16 @@ function efficientSearch(array, item) {
 }
 
 // Random element------------------------------------------------
+//--This function is O(1)
+//--It performs 1 operation regardless the size of input
 function findRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
 // Is it prime?--------------------------------------------------
+//--This functin is O(n)
+//--It performs a check on all numbers up to number to see if it's prime
+//--Example (23 gets 23 operations) (41 gets 41 operations)
 function isPrime(n) {
   // if n is less than 2 or a decimal, it's not prime
   if (n < 2 || n % 1 !== 0) {
